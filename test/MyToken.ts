ce07a1fc2b650c5d80d613fe2fbab5c972f6a459
Mohammad Amir <amir@connextar.com>
2024-11-04 22:09:02 +0000

    Started adding tests for MyToken.

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -11,11 +11,12 @@ const deployContract = async () => {
   const deployerAddress = deployer!.account.address;
   const account2Address = account2!.account.address;
   const contract = await viem.deployContract(CONTRACT_NAME);
-  return {publicClient, deployer, deployerAddress, account2, account3, account2Address, contract};
+  return { publicClient, deployer, deployerAddress, account2, account3, account2Address, contract };
 };
 
 const transferFixture = async (publicClient: PublicClient, contract: any, toAddress: string, amount: bigint) => {
   const { deployer } = await loadFixture(deployContract);
+  // Execute the transfer transaction
   // https://hardhat.org/hardhat-runner/plugins/nomicfoundation-hardhat-viem#contracts
   const txHash = await contract.write.transfer([toAddress, amount]);
   // https://viem.sh/docs/actions/public/getTransactionReceipt#gettransactionreceipt
@@ -23,12 +24,13 @@ const transferFixture = async (publicClient: PublicClient, contract: any, toAddr
   // console.log("Ballot -> giveRightToVoteFixture -> txHash", txHash, "receipt", receipt);
   // https://viem.sh/docs/glossary/terms#transaction-receipt
   expect(receipt.status).to.equal("success");
-
-  const withdrawalEvents = await contract.getEvents.Transfer();
-  expect(withdrawalEvents).to.have.lengthOf(1);
-  expect(withdrawalEvents[0].args.from?.toLowerCase()).to.equal(deployer!.account.address);
-  expect(withdrawalEvents[0].args.to?.toLowerCase()).to.equal(toAddress);
-  expect(withdrawalEvents[0].args.value).to.equal(1n);
+  // Get the events from the transfer transaction
+  const events = await contract.getEvents.Transfer();
+  console.log("MyToken -> transferFixture -> events", events);
+  expect(events).to.have.lengthOf(1);
+  expect(events[0].args.from?.toLowerCase()).to.equal(deployer!.account.address);
+  expect(events[0].args.to?.toLowerCase()).to.equal(toAddress);
+  expect(events[0].args.value).to.equal(amount);
 };
 
 describe(CONTRACT_NAME, async () => {
